c4605d45100af7a1e5eb79974d5de95d12c42f6d
Onsymers <omarfarouk.732@gmail.com>
2019-04-08 22:26:17 +0200

    First Commit

---
+++ src/compiler/Compiler.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiler;
+import GUI.Frame;
+
+/**
+ *
+ * @author omarf
+ */
+public class Compiler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Frame frame=new Frame();
+        
+        frame.setVisible(true);
+        
+        
+//        String x="omar";
+//        
+//        System.out.println(x.length());
+        
+        
+        
+        
+        
+    }
+    
+}
